bdea0f2b74ab1cfd0a2f449e8c39aabcabee9285
Fahim Alif <fahimalif077@gmail.com>
2025-04-27 01:33:07 +0600

    updated create event

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   // !! WARN !!
-   ignoreBuildErrors: true,
-  //  images:{
-  //   remotePatterns:[new URL('https://portfolio-website-snowy.vercel.app/_next/image?url=%2Fheroimg.svg&w=1080&q=75'),new URL('https://res.cloudinary.com/dswzdpmqj/image/upload/')]
-  //  }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['portfolio-website-snowy.vercel.app','res.cloudinary.com'],
   },
